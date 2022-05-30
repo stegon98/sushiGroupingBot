4fce6a3375a5afec7e5544ecc3a13eb28145437b
stegon <stefano.gonella98@gmail.com>
2022-05-30 21:58:35 +0200

    risposte funzionanti

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ import psycopg2
 import os
 from bestemmie import Bestemmie
 import telegram
+from telegram.ext import Updater, CommandHandler
 
 token=os.environ['TOKEN_ID']
+chat_ids = set()
+
+def start(update, context):
+    chat_id = update.effective_chat.id
+    update.message.reply_text("start")
+    print("start called from chat with id = {}".format(chat_id))
+
+def add(update, context):
+    chat_id = update.effective_chat.id
+    update.message.reply_text("add")
+    print("adddaaa".format(chat_id))
+
+def end(update, context):
+    chat_id = update.effective_chat.id
+    chat_ids.remove(chat_id)
+
+
 
 def logUsingPorchiddei(error):
     porcone = Bestemmie()
@@ -12,8 +30,16 @@ def logUsingPorchiddei(error):
 
 if __name__ == '__main__':
     bot = telegram.Bot(token)
-
+    start
     print(bot.get_me())
+
+    updater = Updater(token)
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("end", end))
+    updater.dispatcher.add_handler(CommandHandler("add", add))
+    updater.start_polling()
+    updater.idle()
+
     try:
         connection = psycopg2.connect(user=os.environ['USER_DB'],
                                       password=os.environ['PASS_DB'],
