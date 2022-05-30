3153b1dee894000332c7584b82d9f544aa26337d
edoshin98 <edosc998@gmail.com>
2022-05-30 23:28:12 +0200

    Inserimento funzioni CRUD

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import psycopg2
 import os
-import json
 from bestemmie import Bestemmie
 import telegram
 from telegram.ext import Updater, CommandHandler
@@ -9,22 +8,15 @@ import database
 token = os.environ['TOKEN_ID']
 chat_ids = set()
 
-class Order:
-  def __init__(mysillyobject, username, parameters):
-    mysillyobject.username = username
-    mysillyobject.parameters = parameters
-
-  def insertOrder(order):
-    print("Hello my name is " + order.username +" "+order.parameters[1])
-
 def extract_number(text, username):
     paramList=[]
     for i in range(len(text.split())):
-        paramList.append(text.split()[i].strip())
+        if(i == 0):
+            paramList.append(username)
+        elif(i>0 & text.split()[i].strip() != "/add"):
+            paramList.append(text.split()[i].strip())
 
-    order=Order(username, paramList)
-
-    order.insertOrder()
+    database.insertData(paramList)
 
 def logUsingPorchiddei(error):
     porcone = Bestemmie()
@@ -45,15 +37,40 @@ def add(update, context):
 def end(update, context):
     chat_id = update.effective_chat.id
     chat_ids.remove(chat_id)
+#
+# TODO: implementare le query per le seguenti istruzioni da db
+#
+def myDishes(update, context):
+    # funzione che estrarra solo i piatti ordinati da un utente
+    user = update.message.from_user
+    database.getMyDishes(user["username"])
 
+def allDishes(update, context):
+    # funzione che estrarra tutti i piatti ordinati
+    database.geAllDishes()
+
+def removeDish(update, context):
+    # funzione per la rimozione del piatto inserito
+    database.removeDish()
+
+def removeAllDishes(update, context):
+    # funzione per rimuovere tutti i piatti di un utente
+    database.removeAllDishes()
+
+def removeAll(update, context):
+    # funzione di cancellazione di tutti i piatti
+    database.removeAll()
 
 if __name__ == '__main__':
     bot = telegram.Bot(token)
-    print(bot.get_me())
-    database.insertData(["DROP TABLE BANANA;TRUNCATE TABLE SI;SELECT * FROM DUAL"])
     updater = Updater(token)
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("end", end))
     updater.dispatcher.add_handler(CommandHandler("add", add))
+    updater.dispatcher.add_handler(CommandHandler("myDishes", myDishes))
+    updater.dispatcher.add_handler(CommandHandler("allDishes", allDishes))
+    updater.dispatcher.add_handler(CommandHandler("remove", removeDish))
+    updater.dispatcher.add_handler(CommandHandler("removeAll", removeAllDishes))
+    updater.dispatcher.add_handler(CommandHandler("truncate", removeAll))
     updater.start_polling()
     updater.idle()
